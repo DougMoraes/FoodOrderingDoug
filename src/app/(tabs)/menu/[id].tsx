bc1cba3b59e8f2446e5cca580b1458342aa17710
DougMoraes <dougmoraes91@gmail.com>
2024-07-15 10:35:53 +0200

    feat: initial design for details screen

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,13 +1,46 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 import React from 'react';
-import { useLocalSearchParams } from 'expo-router';
+import { Stack, useLocalSearchParams } from 'expo-router';
+import products from 'assets/data/products';
+import Constants from '@/constants/Constants';
 
 export default function ProductDetailsScreen() {
   const {id} = useLocalSearchParams();
+  const product = products.find(p => p.id.toString() === id);
+
+  if(!product) {
+    return (
+      <View>
+        <Stack.Screen options={{ title: 'Item Not Found'}}/>
+        <Text>Product not found!</Text>
+      </View>
+    )
+  }
 
   return (
-    <View>
-      <Text>product id: {id}</Text>
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product.name}}/>
+      <Image
+        source={{ uri: product.image || Constants.defaultPizzaImage}}
+        style={styles.image}
+      />
+      <Text style={styles.price}> ${product.price}</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+    padding: 10
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+  },
+  price: {
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+});
