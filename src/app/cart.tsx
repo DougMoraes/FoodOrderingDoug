6762f8378f597bf4997aa736e8d9f84451bff3fb
DougMoraes <dougmoraes91@gmail.com>
2024-07-31 16:58:20 +0200

    feat: render CartListItems list

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,16 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { Platform } from 'react-native';
-import { Text, View } from '@/components/Themed';
+import { Platform, FlatList } from 'react-native';
+import { View } from '@/components/Themed';
 import { useCart } from '@/providers/CartProvider';
+import CartListItem from '@/components/CartListItem';
 
 export default function CartScreen() {
-
   const {items} = useCart();
 
   return (
     <View >
-      <Text>Cart: {items.length}</Text>
-
+      <FlatList
+        data={items}
+        renderItem={({item}) => <CartListItem cartItem={item}/>}
+        contentContainerStyle={{padding: 10, gap: 10}}
+      />
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
   );
