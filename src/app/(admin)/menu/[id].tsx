9ccfb8605d73ad64c5f84137b900a2405b9a9c20
DougMoraes <dougmoraes91@gmail.com>
2024-08-05 12:39:51 +0200

    refac: move admin create screen config

--- src/app/(admin)/menu/[id].tsx
+++ src/app/(admin)/menu/[id].tsx
@@ -1,7 +1,9 @@
-import { View, Text, Image, StyleSheet } from 'react-native';
-import { Stack, useLocalSearchParams } from 'expo-router';
+import { View, Text, Image, StyleSheet, Pressable } from 'react-native';
+import { Link, Stack, useLocalSearchParams } from 'expo-router';
 import products from 'assets/data/products';
 import Constants from '@/constants/Constants';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import Colors from '@/constants/Colors';
 
 export default function ProductDetailsScreen() {
   const {id} = useLocalSearchParams();
@@ -18,7 +20,25 @@ export default function ProductDetailsScreen() {
 
   return (
     <View style={styles.container}>
-      <Stack.Screen options={{ title: product.name}}/>
+      <Stack.Screen
+        options={{
+        title: 'Menu',
+        headerRight: () => (
+          <Link href={`/(admin)/menu/create?id=${id}`} asChild>
+            <Pressable>
+              {({ pressed }) => (
+                <FontAwesome
+                  name="plus-square-o"
+                  size={25}
+                  color={Colors.light.tint}
+                  style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                />
+              )}
+            </Pressable>
+          </Link>
+        ),
+        }}
+      />
       <Image
         source={{ uri: product.image || Constants.defaultPizzaImage}}
         style={styles.image}
