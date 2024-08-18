5c51b69ceca1ea4d8f5df0e86a4621ac92fe6490
DougMoraes <dougmoraes91@gmail.com>
2024-08-18 09:07:19 +0200

    feat: add ordersScreen to admin

--- src/app/(admin)/menu/_layout.tsx
+++ src/app/(admin)/menu/_layout.tsx
@@ -1,25 +1,39 @@
 import Colors from '@/constants/Colors';
 import { FontAwesome } from '@expo/vector-icons';
 import { Link, Stack } from 'expo-router';
-import { Pressable } from 'react-native';
+import { Pressable, View } from 'react-native';
 
 export default function MenuLayout() {
   return <Stack>
     <Stack.Screen name="index" options={{
       title: 'Menu',
       headerRight: () => (
-        <Link href="/(admin)/menu/create" asChild>
-          <Pressable>
-            {({ pressed }) => (
-              <FontAwesome
-                name="plus-square-o"
-                size={25}
-                color={Colors.light.tint}
-                style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-              />
-            )}
-          </Pressable>
-        </Link>
+        <View style={{display: 'flex', flexDirection: 'row'}}>
+          <Link href="/(admin)/menu/create" asChild>
+            <Pressable>
+              {({ pressed }) => (
+                <FontAwesome
+                  name="plus-square-o"
+                  size={25}
+                  color={Colors.light.tint}
+                  style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                />
+              )}
+            </Pressable>
+          </Link>
+          <Link href="/orders" asChild>
+            <Pressable>
+              {({ pressed }) => (
+                <FontAwesome
+                  name="book"
+                  size={25}
+                  color={Colors.light.tint}
+                  style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                />
+              )}
+            </Pressable>
+          </Link>
+        </View>
       ),
       }}/>
   </Stack>;
---
+++ src/app/(admin)/orders/[id].tsx
@@ -0,0 +1,48 @@
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { Stack, useLocalSearchParams } from 'expo-router';
+import orders from 'assets/data/orders';
+import OrderItem from '@/components/OrderItem';
+import OrderListItem from '@/components/OrderListItem';
+
+
+export default function ProductDetailsScreen() {
+  const {id} = useLocalSearchParams();
+  const order = orders.find(o => o.id.toString() === id);
+
+  if(!order) {
+    return (
+      <View>
+        <Stack.Screen options={{ title: 'Order Not Found'}}/>
+        <Text>Order not found!</Text>
+      </View>
+    )
+  }
+
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: `Order #${order.id}`}}/>
+      <FlatList
+        data={order.order_items}
+        renderItem={({item}) => <OrderItem item={item}/>}
+        contentContainerStyle={{gap: 10, padding: 10}}
+        ListHeaderComponent={<OrderListItem order={order}/>}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+    padding: 10
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+  },
+  price: {
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+});
