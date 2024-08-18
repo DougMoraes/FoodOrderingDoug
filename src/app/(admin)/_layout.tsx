85c76afa7fdf6713a58cfc57c6d6a2099169fe78
DougMoraes <dougmoraes91@gmail.com>
2024-08-18 14:53:50 +0200

    feat: remove headers

--- src/app/(admin)/_layout.tsx
+++ src/app/(admin)/_layout.tsx
@@ -41,7 +41,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="orders"
         options={{
-          title: 'Orders',
+          headerShown: false,
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
