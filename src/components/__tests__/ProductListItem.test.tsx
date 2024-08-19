c5df5aa4199d2f7d7ae34fcaf894f9b81a7c624f
DougMoraes <dougmoraes91@gmail.com>
2024-08-20 00:25:32 +0200

    test: add ProductListItem render test

---
+++ src/components/__tests__/ProductListItem.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react-native';
+import { getAsRegExp } from './__utils__/utils';
+import { mockProduct } from './__utils__/data';
+import { renderRouter } from 'expo-router/testing-library';
+import ProductListItem from '../ProductListItem';
+
+describe('ProductListItem', () => {
+  test('shoud render', async () => {
+    // This test is causing the warning:
+    // Warning: An update to ForwardRef(NavigationContainerInner) inside a test was not wrapped in act(...).
+    // When testing, code that causes React state updates should be wrapped into act(...):
+
+    // Using act around the rederers returned an error.
+
+    renderRouter();
+    render(<ProductListItem product={mockProduct}/>)
+
+    const renderedName = screen.getAllByText(getAsRegExp(mockProduct.name));
+    const renderedPrice = screen.getAllByText(getAsRegExp(mockProduct.price));
+
+    expect(renderedName.length).toBe(1);
+    expect(renderedPrice.length).toBe(1);
+  });
+});
