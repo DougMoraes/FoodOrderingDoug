86ae99b17d0b2521510fb9ea8e66a65382f19dd8
DougMoraes <dougmoraes91@gmail.com>
2024-08-19 17:21:11 +0200

    test: cleanup  OrderItem test

--- src/components/__tests__/OrderItem.test.tsx
+++ src/components/__tests__/OrderItem.test.tsx
@@ -1,21 +1,14 @@
 import { render, screen } from '@testing-library/react-native';
 import OrderItem from '@/components/OrderItem';
-import Constants from '@/constants/Constants';
-import dayjs from 'dayjs';
 import { PizzaSize } from '@/types';
+import { getAsRegExp } from './__utils__/utils';
+import { mockProduct } from './__utils__/data';
 
 test('shoud render OrderItem', async () => {
-  const now = dayjs();
-
   const orderMock = {
     id: 1,
     product_id: 1,
-    products: {
-      id: 1,
-      image: Constants.defaultPizzaImage,
-      name: 'TestPizza',
-      price: 25,
-    },
+    products: mockProduct,
     order_id: 1,
     size: 'L' as PizzaSize,
     quantity: 1,
@@ -23,10 +16,10 @@ test('shoud render OrderItem', async () => {
 
   render(<OrderItem item={orderMock}/>)
 
-  const renderedName = screen.getAllByText(orderMock.products.name);
-  const renderedPrice = screen.getAllByText(`$${orderMock.products.price}`);
-  const renderedSize = screen.getAllByText(`Size: ${orderMock.size}`);
-  const renderedQuantity = screen.getAllByText(`Qtd: ${orderMock.quantity}`);
+  const renderedName = screen.getAllByText(getAsRegExp(orderMock.products.name));
+  const renderedPrice = screen.getAllByText(getAsRegExp(orderMock.products.price));
+  const renderedSize = screen.getAllByText(getAsRegExp(orderMock.size));
+  const renderedQuantity = screen.getAllByText(getAsRegExp(orderMock.quantity));
 
   expect(renderedName.length).toBe(1);
   expect(renderedPrice.length).toBe(1);
