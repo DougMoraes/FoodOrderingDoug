9d67246d374c33a205fe5f49ad265e6af9e3a4f7
DougMoraes <dougmoraes91@gmail.com>
2024-08-20 00:25:44 +0200

    test: add StatusSelector render test

---
+++ src/components/__tests__/StatusSelector.test.tsx
@@ -0,0 +1,18 @@
+import { fireEvent, render, screen } from "@testing-library/react-native";
+import StatusSelector from "../StatusSelector";
+
+describe('StatusSelector', () => {
+  test('should call onPressSelect when option is clicked', () => {
+    const mockOnPress = jest.fn();
+
+    render(<StatusSelector onPressSelect={mockOnPress} selectedStatus="Delivered"/>)
+
+    const renderedOption = screen.getAllByText('Delivering');
+
+    expect(renderedOption.length).toBe(1);
+
+    fireEvent.press(renderedOption[0]);
+
+    expect(mockOnPress).toHaveBeenCalled();
+  })
+});
