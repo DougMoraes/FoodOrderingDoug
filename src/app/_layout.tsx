e6b51992a5d85d005d83de6b40ba836228fe8c7d
DougMoraes <dougmoraes91@gmail.com>
2024-08-21 16:10:56 +0200

    fix: fix CartProvider import

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -8,7 +8,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 
 import { useColorScheme } from '@/components/useColorScheme';
-import CartProvider from '@/providers/CartProvider';
+import { CartProvider } from '@/providers/CartProvider';
 
 export {
   // Catch any errors thrown by the Layout component.
